f21de88e4af95929e9bec46c1ce45d48c246d5d9
Shubham Raut <shubhamrajkumar.raut@zemosolabs.com>
2022-01-28 19:50:02 +0530

    made changes6

---
+++ src/Components/organisms/Card/index.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import BookCard from ".";
+import renderer from 'react-test-renderer';
+import {BrowserRouter} from 'react-router-dom'
+const props = {
+    id: 0,
+    imgHeight: 240,
+    url: '/assets/book.png',
+    bookName: "Being Boss",
+    writerName: "Kathleen Shannon and Emily...",
+    timeRead: "13-minute read",
+    width: 280,
+    inLibrary: false,
+    libraryHandler:false,
+    cardId:1
+};
+
+test('BookCardcomponent', () => {
+    const component = renderer.create(
+        <BrowserRouter>
+            <BookCard {...props}></BookCard>
+        </BrowserRouter>
+    );
+    expect(component).toMatchSnapshot();
+});
